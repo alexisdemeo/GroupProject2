2c07f73907c55b1f943c1eed2ea70c760051b1b4
Jillian Pason <pasonjill@myvuw.ac.nz>
2020-02-02 12:30:32 +1300

    Added basic setup

--- widget/re_widget.js
+++ widget/re_widget.js
@@ -1,7 +1,40 @@
-// Display re widget on scroll
-window.addEventListener("scroll", myFunction);
 
-function myFunction() {
-    let widget = document.getElementById("re-widget")
-    widget.classList.remove('is-hidden');
+let site = "";
+let article = "";
+let positive_sentiment = "";
+let category = "";
+let comments = "";
+let email = "";
+
+const popup = document.getElementById("popup");
+console.log(popup); 
+
+// Display re widget on scroll
+// window.addEventListener("scroll", myFunction);
+
+// function myFunction() {
+//     let widget = document.getElementById("re-widget")
+//     widget.classList.remove('is-hidden');
+// }
+
+const btn = document.querySelector("#submit")
+
+async function getFromS3() {
+    console.log("Fired")
+    await fetch('https://sample-form-bucket.s3-ap-southeast-2.amazonaws.com/index.html', {
+    }).then((response) => {
+        console.log("Response from S3")
+        // const res = response.text()
+
+        // console.log(response.json())
+        // return response.json();
+        return response.text()
+
+    }).then((text) => {
+        console.log(text)
+        popup.innerHTML = "";
+        popup.innerHTML = text;
+    })
 }
+
+btn.addEventListener("click", getFromS3)
