9ecc8e8e9f7124f14b4daa8a7320ed2ba6b44875
Sanjay Sudhakaran <34014570+SanjayS5@users.noreply.github.com>
2020-02-04 10:05:37 +1300

    Created payload

--- widget/re_widget.js
+++ widget/re_widget.js
@@ -1,13 +1,13 @@
 
-let site = "";
-let article = "";
+let site = window.location.origin;
+let article = window.location.href;
 let positive_sentiment = "";
 let category = "";
 let comments = "";
 let email = "";
 
 const popup = document.querySelector("#popup");
-console.log(popup); 
+console.log(popup);
 
 // ************ Event Listeners *************
 // same idea as using arrow notation
@@ -17,23 +17,47 @@ console.log(popup);
 
 const getSentiment = (e) => {
     console.log(e.srcElement.offsetParent.value);
-    positive_sentiment = e.srcElement.offsetParent.value; 
+    positive_sentiment = e.srcElement.offsetParent.value;
     console.log("Positive sentiment: " + positive_sentiment);
 }
 
-const loadFormFunctions = () => {
-        // const likeBtn = document.querySelector(".re-widget-btn-round-like");
-        // const dislikeBtn = document.querySelector(".re-widget-btn-round-dont");
-        // likeBtn.addEventListener("click", getSentiment);
-        // dislikeBtn.addEventListener("click", getSentiment); 
-        const btnWrapper = document.querySelector(".re-widget-btn-wrapper-f1");
-        btnWrapper.addEventListener("click", getSentiment);
-
-        // Send data to API Gateway
-
-        
-
+const createPayload = () => {
+    payload = {
+        site: site,
+        article: article,
+        positive_sentiment: positive_sentiment,
+        category: category,
+        comments: comments,
+        email: email
     }
+    return payload
+}
+
+const loadFormFunctions = () => {
+    // const likeBtn = document.querySelector(".re-widget-btn-round-like");
+    // const dislikeBtn = document.querySelector(".re-widget-btn-round-dont");
+    // likeBtn.addEventListener("click", getSentiment);
+    // dislikeBtn.addEventListener("click", getSentiment); 
+    const btnWrapper = document.querySelector(".re-widget-btn-wrapper-f1");
+    btnWrapper.addEventListener("click", getSentiment);
+
+    // Generate payload
+
+    const payload = createPayload()
+
+    // Send data to API Gateway
+    fetch('https://sso03h7hyg.execute-api.ap-southeast-2.amazonaws.com/dev/feedback', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+
+}
+
+
 
 
 const btn = document.querySelector("#submit")
@@ -43,10 +67,10 @@ const btn = document.querySelector("#submit")
 // ******** loadFormFunctions needs to happen after async code ***********
 
 async function getFromS3() {
-   
+
     console.log("Fired")
     await fetch('https://sample-form-bucket.s3-ap-southeast-2.amazonaws.com/re_widget_f1.html', {
-    }).then((response) => { 
+    }).then((response) => {
         console.log("Response from S3")
         return response.text()
 
@@ -54,9 +78,9 @@ async function getFromS3() {
         console.log(text)
         popup.innerHTML = "";
         popup.innerHTML = text;
-        loadFormFunctions(); 
+        loadFormFunctions();
     })
-    
+
 }
 
 btn.addEventListener("click", getFromS3);
